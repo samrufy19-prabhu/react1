ea6b3024b3c45fbaa29e53f2b096922a0ad3ba08
sam ruby <samrufy19@gmail.com>
2026-09-13 00:06:17 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-
 import React from "react";
-
-function Welcome() {
-  const isLoggedIn = true;
-
-  return (
-    <div>
-      {isLoggedIn ? (
-        <h2>Welcome User</h2>
-      ) : (
-        <h2>Please Login</h2>
-      )}
-    </div>
-  );
-}
-
-export default Welcome;
+import { useState } from "react"; 
+function TodoList() { 
+  const [todos, setTodos] = useState([ 
+  { id: 1, task: "Learn React", completed: false }, 
+    { id: 2, task: "Build a project", completed: true }, 
+  ]); 
+  const toggleComplete = (id) => { 
+    setTodos( 
+      todos.map((todo) => 
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo 
+      ) 
+    ); 
+  }; 
+  return ( 
+    <ul> 
+      {todos.map((todo) => ( 
+        <li 
+          key={todo.id} 
+          style={{ textDecoration: todo.completed ? "line-through" : "none" }} 
+          onClick={() => toggleComplete(todo.id)} 
+        > 
+          {todo.task} 
+        </li> 
+      ))} 
+    </ul> 
+  ); 
+} 
+export default TodoList;
