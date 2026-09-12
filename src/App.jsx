881a3a3b9c42d6b19f0e6c86514c38e9a0e4faf4
sam ruby <samrufy19@gmail.com>
2026-09-12 23:55:52 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,18 @@
-import { useState } from "react"; 
-function TodoList() { 
-  const [todos, setTodos] = useState([]); 
-  const [input, setInput] = useState(""); 
-  const addTodo = () => {
-       if (input.trim()) { 
-      setTodos([...todos, { id: Date.now(), task: input, completed: false }]); 
-      setInput(""); 
-    } 
-  }; 
-  return ( 
-    <div> 
-      <input 
-        type="text" 
-        value={input} 
-        onChange={(e) => setInput(e.target.value)} 
-        placeholder="Add a new task" 
-      /> 
-      <button onClick={addTodo}>Add</button> 
-      <ul> 
-        {todos.map((todo) => ( 
-          <li key={todo.id}>{todo.task}</li> 
-        ))} 
-      </ul> 
-    </div> 
-  ); 
-} 
-export default TodoList;
+
+import React from "react";
+
+function Welcome() {
+  const isLoggedIn = true;
+
+  return (
+    <div>
+      {isLoggedIn ? (
+        <h2>Welcome User</h2>
+      ) : (
+        <h2>Please Login</h2>
+      )}
+    </div>
+  );
+}
+
+export default Welcome;
